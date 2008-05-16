8b31e4ad7cf74e3f30c144d867fdd2a239bbc3fe
iutinvg <iutinvg@bd628be6-a245-0410-a438-c9fbef427127>
2008-05-16 10:49:21 +0000

    log helper is added
    git-svn-id: https://mvcskel.googlecode.com/svn/trunk@12 bd628be6-a245-0410-a438-c9fbef427127

---
+++ lib/pear/MvcSkel/Helper/Log.php
@@ -0,0 +1,51 @@
+<?php
+/**
+* MvcSkel controller.
+*
+* PHP versions 5
+*
+* @category   framework
+* @package    MvcSkel
+* @copyright  2008, Whirix Ltd.
+* @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser Public General License (LGPL).
+* @link       http://code.google.com/p/mvcskel/
+*/
+
+/**
+* Include PEAR Log library.
+*/
+    require_once 'Log.php';
+    
+    /**
+     * Config helper is required to read Logger setup.
+     */
+    require_once 'MvcSkel/Helper/Config.php';
+
+/**
+ * Log helper. 
+ * 
+ * Just shorthand wrapper for PEAR::Log package.
+ *  Usage:
+ *  <code>
+ *    $log = new MvcSkel_Helper_Log::get();
+ *    $log->debug('some test message');
+ *  </code>
+ *    
+ * @package    MvcSkel
+ * @subpackage    Helper
+ */
+    class MvcSkel_Helper_Log {
+        /**
+         * Log instance getter.
+         * Wrapper for Log::singleton method.
+         * @param string $logId log messages id, default is 'MvcSkel'
+         * @return PEAR::Log object instance
+         */
+        public function get($logId = 'MvcSkel') {
+            $config = MvcSkel_Helper_Config::read();
+            return Log::singleton($config['logger']['handler'],
+                $config['logger']['name'],
+                $logId, array(), $config['logger']['level']);
+        }
+    }
+?>
