688942aa27e2384982f9c5bfd290350178619364
iutinvg <iutinvg@bd628be6-a245-0410-a438-c9fbef427127>
2009-06-19 07:42:10 +0000

    polish sql-script
    git-svn-id: https://mvcskel.googlecode.com/svn/trunk@75 bd628be6-a245-0410-a438-c9fbef427127

--- docs/mvcskel.sql
+++ docs/mvcskel.sql
@@ -1,37 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 2.8.1
--- http://www.phpmyadmin.net
--- 
--- Host: localhost:3306
--- Generation Time: Mar 06, 2007 at 10:40 AM
--- Server version: 3.23.53
--- PHP Version: 4.3.11
--- 
--- Database: `mvcskel`
--- 
-
--- --------------------------------------------------------
-
--- 
--- Table structure for table `User`
--- 
-
-CREATE TABLE `User` (
-  `id` int NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `User` (
+  `id` int(11) NOT NULL auto_increment,
   `username` varchar(255) NOT NULL default '',
   `password` varchar(255) NOT NULL default '',
   `roles` varchar(255) NOT NULL default '',
   `email` varchar(255) NOT NULL default '',
   `fname` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`id`),
-  UNIQUE KEY `username` (`username`)
-) TYPE=MyISAM;
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+);
 
--- 
--- Dumping data for table `User`
--- 
-
-INSERT INTO `User` (`id`, `username`, `password`, `roles`, `email`, `fname`) 
-VALUES 
-(1, 'user', md5('user'), 'User', 'user@noemail.org', 'First User'),
-(2, 'admin', md5('admin'), 'Administrator', 'admin@noemail.org', 'Administrator');
+INSERT INTO `User` (`id`, `username`, `password`, `roles`, `email`, `fname`) VALUES
+(1, 'admin', md5('admin'), 'Administrator', 'admin@noemail.org', 'Fred'),
+(2, 'user', md5('user'), 'User', 'user@noemail.org', 'Joe');
