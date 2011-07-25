efb4071cf3d2ea7f23b07eecef8f8d5a607176a3
iutinvg <iutin@whirix.com>
2011-07-25 18:20:52 +0700

    very useful function for URL_Helper

--- lib/MvcSkel/Helper/Url.php
+++ lib/MvcSkel/Helper/Url.php
@@ -1,62 +1,63 @@
 <?php
-/**
-* MvcSkel url helper.
-*
-* PHP versions 5
-*
-* @category   framework
-* @package    MvcSkel
-* @copyright  2008, Whirix Ltd.
-* @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser Public General License (LGPL).
-* @link       http://code.google.com/p/mvcskel/
-*/
 
 /**
-* Include PEAR Log library.
-*/
+ * MvcSkel url helper.
+ *
+ * PHP versions 5
+ *
+ * @category   framework
+ * @package    MvcSkel
+ * @copyright  2008, Whirix Ltd.
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser Public General License (LGPL).
+ * @link       http://code.google.com/p/mvcskel/
+ */
+/**
+ * Include PEAR Log library.
+ */
 require_once 'Log.php';
 
 /**
-* Url helper. 
-* 
-* Builds urls.
-* 
-* @package    MvcSkel
-* @subpackage    Helper
-*/
+ * Url helper. 
+ * 
+ * Builds urls.
+ * 
+ * @package    MvcSkel
+ * @subpackage    Helper
+ */
 class MvcSkel_Helper_Url {
+
     /**
-    * Url builder.
-    * Usage:
-    *  <code>
-    *    echo MvcSkel_Helper_Url::url('Main/Index', array('p1'=>'a'), 'anch');
-    *    // displays:
-    *    // /mvcskel/Main/Index/p1/a#anch
-    *  </code>
-    * @param string $url controller/action format path
-    * @param array $array of query values ('p1'=>'a', 'p2'=>'1'), null by default
-    * @param string $anchor anchor, empty by default
-    * @return string target URL
-    */
+     * Url builder.
+     * Usage:
+     *  <code>
+     *    echo MvcSkel_Helper_Url::url('Main/Index', array('p1'=>'a'), 'anch');
+     *    // displays:
+     *    // /mvcskel/Main/Index/p1/a#anch
+     *  </code>
+     * @param string $url controller/action format path
+     * @param array $array of query values ('p1'=>'a', 'p2'=>'1'), null by default
+     * @param string $anchor anchor, empty by default
+     * @return string target URL
+     */
     public static function url($url, $params = null, $anchor = '') {
         $url = strtolower($url);
         $config = MvcSkel_Helper_Config::read();
-        if ($params==null) {
+        if ($params == null) {
             $params = array();
         }
 
-        if (isset($config['nice-urls']) && $config['nice-urls']==true) {
+        if (isset($config['nice-urls']) && $config['nice-urls'] == true) {
             /*
              * Search engine friendly URLs like /Main/Index/p1/a
              */
-            foreach ($params as $k=>$v) {
-                $url .= "/$k/".urlencode($v);
+            foreach ($params as $k => $v) {
+                $url .= "/$k/" . urlencode($v);
             }
             if (!empty($anchor)) {
                 $url .= "#$anchor";
             }
 
-            if (substr($url, 0, 1)!='/') {
+            if (substr($url, 0, 1) != '/') {
                 // add root path
                 $config = MvcSkel_Helper_Config::read();
                 return $config['root'] . $url;
@@ -68,7 +69,7 @@ class MvcSkel_Helper_Url {
             /*
              * Usual URLs like /Main/Index?p1=a
              */
-            if (substr($url, 0, 1)!='/') {
+            if (substr($url, 0, 1) != '/') {
                 $params['mvcskel_redirect_url'] = $url;
                 $config = MvcSkel_Helper_Config::read();
                 $url = $config['root'];
@@ -79,12 +80,12 @@ class MvcSkel_Helper_Url {
         $urlObj = new MvcSkel_Url($url);
         $urlObj->setUseHtmlEntities(false);
         $urlObj->addVars($params);
-        if ($anchor!='') {
+        if ($anchor != '') {
             $urlObj->setAnchor($anchor);
         }
         return $urlObj->construct();
     }
-    
+
     /**
      * Redirect tool.
      * Usage:
@@ -101,16 +102,28 @@ class MvcSkel_Helper_Url {
         session_write_close();
         exit();
     }
-    
+
     /**
-    * Get variable from request (higher priority) 
-    * or session (lower priority), and put it into session
-    * for later usage in further requests.
-    * @param string $varName Variable name
-    * @param mixed $default Default variable value, used if it's
-    *                   not found in request and session
-    * @return mixed Current variable value
-    */
+     * The full url generator.
+     * Similar to url() method in parameters.
+     */
+    public static function absoluteUrl($url, $params = null, $anchor = '') {
+        $prefix = 'http://';
+        if (!empty($_SERVER['HTTPS'])) {
+            $prefix = 'https://';
+        }
+        return $prefix . $_SERVER['SERVER_NAME'] . self::url($url, $params, $anchor);
+    }
+
+    /**
+     * Get variable from request (higher priority) 
+     * or session (lower priority), and put it into session
+     * for later usage in further requests.
+     * @param string $varName Variable name
+     * @param mixed $default Default variable value, used if it's
+     *                   not found in request and session
+     * @return mixed Current variable value
+     */
     public static function getStickyVar($varName, $default) {
         $sessionKey = "{$_REQUEST['mvcskel_c']}_{$_REQUEST['mvcskel_a']}_$varName";
         $value = $default;
@@ -119,10 +132,11 @@ class MvcSkel_Helper_Url {
         } else if (isset($_SESSION[$sessionKey])) {
             $value = $_SESSION[$sessionKey];
         }
-        
+
         $_SESSION[$sessionKey] = $value;
         return $value;
     }
 
 }
+
 ?>
